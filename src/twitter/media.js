25382dadaaf617448cd838b47b855feb863fd4a5
Kevin Jiang <kcj1017@gmail.com>
2022-12-27 03:31:43 -0500

    Adds a file saving feature.

--- src/twitter/media.js
+++ src/twitter/media.js
@@ -1,21 +1,37 @@
 import Downloader from "nodejs-file-downloader";
-import fs from "fs";
-import config from "config";
+import { v4 as uuidv4 } from "uuid";
+import Tweet from "../database/model/tweet.js";
 
-async function saveMedia(mediaURL) {
+import config from "config";
+import logger from "../logging.js";
+
+function saveMedia(tweetID, mediaID, mediaURL, mediaType) {
+  if (mediaType == "preview") saveFile(tweetID, mediaID, mediaURL, "preview");
+  else saveFile(tweetID, mediaID, mediaURL, "filePath");
+}
+
+function saveFile(tweetID, mediaID, mediaURL, key) {
+  //   const field = `media.$.${key}`;
   const directory = config.get("storage.directory");
+  const extension = mediaURL.split("?")[0].split(".").pop();
+  const fileName = `${tweetID}-${mediaID}.${extension}`;
   const downloader = new Downloader({
     url: mediaURL,
     directory: directory,
+    fileName: fileName,
   });
-  try {
-    const { filePath } = await downloader.download();
-    console.log("Download Completed at", filePath);
-    return filePath;
-  } catch (error) {
-    // console.log("Download failed", error);
-  }
-  return "";
+  downloader
+    .download()
+    .then(() => {
+      logger.debug(`Downloaded: ${mediaURL}`);
+      Tweet.updateOne(
+        { _id: tweetID, "media.mediaID": mediaID },
+        {
+          $set: { [`media.$.${key}`]: fileName },
+        }
+      );
+    })
+    .catch(() => logger.error(`Failed to download:  ${mediaURL}`));
 }
 
 function findBestVideoSource(videoSources) {
@@ -35,22 +51,27 @@ function findBestVideoSource(videoSources) {
   return bestVideoSource;
 }
 
-async function extractMedia(mediaInfo) {
+function extractMedia(tweetID, mediaInfo) {
+  const mediaID = uuidv4();
   if (mediaInfo.type == "photo") {
+    saveMedia(tweetID, mediaID, mediaInfo.media_url, "image");
     return {
+      mediaID: mediaID,
       media_type: "image",
       url: mediaInfo.media_url,
-      file_path: await saveMedia(mediaInfo.media_url),
+      file_path: "",
     };
   } else if (mediaInfo.type == "video") {
     let bestVideoSource = findBestVideoSource(mediaInfo.video_info.variants);
-
+    saveMedia(tweetID, mediaID, bestVideoSource, "video");
+    saveMedia(tweetID, mediaID, mediaInfo.media_url, "preview");
     return {
-      media_type: "video",
+      mediaID: mediaID,
+      mediaType: "video",
       url: bestVideoSource,
-      filePath: await saveMedia(bestVideoSource),
+      filePath: "",
       preview: mediaInfo.media_url,
-      preview_file_path: await saveMedia(mediaInfo.media_url),
+      preview_file_path: "",
     };
   }
 }
